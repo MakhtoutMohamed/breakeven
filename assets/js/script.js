f1d408d1d7d5ab498f31a74a319226e5ec07b18e
Mohamed Makhtout <medmakhtout@gmail.com>
2022-10-29 15:16:43 +0100

    day 29

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,3 +57,39 @@ const activeElemOnScroll = function () {
 
 addEventOnElem(window, "scroll", activeElemOnScroll);
 
+//counter
+
+// function animateValue(obj, start, end, duration) {
+//     let startTimestamp = null;
+//     const step = (timestamp) => {
+//       if (!startTimestamp) startTimestamp = timestamp;
+//       const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+//       obj.innerHTML = Math.floor(progress * (start - end) + start);
+//       if (progress < 1) {
+//         window.requestAnimationFrame(step);
+//       }
+//     };
+//     window.requestAnimationFrame(step);
+//   }
+  
+//   const obj = document.getElementById("num");
+//   animateValue(obj, 11, 0, 5000);
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 3000;
+
+valueDisplays.forEach((valueDisplays) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplays.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
+
+
+
